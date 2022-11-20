562f6dc0f904a583b07775c11eee2fb48fbda041
garretts-hub <garrett.k.simon.mil@mail.mil>
2022-11-20 11:44:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Snow Days
+Welcome to Snow Days! This is a fun little project graphically displaying the first snow dates per winter season for reporting weather stations throughout the continental United States. You can clone this repository and run it locally, or you can run it online using  nbviewer.<br>
+
+The `Snow Days Display.ipynb` notebook allows you to select a winter season (indicated by the year at the start of winter) and generate an interactive map of stations that have observed snow by the date. Use the slider to progress the map in time.:<br>
+<img src="https://github.com/garretts-hub/Snow Days/blob/main/images/map_sample.PNG" alt="Stations having observed snow prior to Dec. 19, 2019." title="Sample Map" width=auto>
+<br><br>
+For this notebook to work, your Jupyter environment must have the following packages installed:
+- pandas
+- geopandas
+- shapely
+- ipywidgets
+<br><br>
+All weather data for this notebook was obtained using the Global Historical Climatology Network daily (GHCNd) dataset. This publicly available dataset integrates daily climate summaries from land stations around the globe for more than a century: 
+https://www.ncei.noaa.gov/products/land-based-station/global-historical-climatology-network-daily 
+
+
+
+"# Snow-Days" 
---
+++ snow_trends.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import datetime
+from shapely.geometry import Point, Polygon
+import ipywidgets
+
+#Read the geopandas shape file for continental US states
+states = gpd.read_file('data/shapefile-data/usa-states-census-2014.shp')
+
+#Load the two csvs with (1) date of first snow for each station, and (2) the details of each station
+# Merge them into one dataframe with an inner join.
+def generate_merged_dataframe(year):
+    firstSnowDF = pd.read_csv(f"data/Snow_Trends_Data/First-Snow-Dates/{year}.csv").set_index("id")
+    stationsDF = pd.read_csv("data/Snow_Trends_Data/stations.csv").set_index("id")
+    mergedDF = pd.merge(firstSnowDF, stationsDF, left_index=True, right_index=True)[['date', 'latitude', 'longitude', 'name']]
+    mergedDF['date'] = pd.to_datetime(mergedDF.date)
+    return mergedDF
+
+def generate_geodataframe(mergedDF):    
+    #defined coordinate reference system. Use the same as the states shapefile.
+    crs='epsg:4326'
+    #define a geometry consisting of Points, to use as the GDF geometry
+    geometry=[Point(longlat_tuple) for longlat_tuple in zip(mergedDF['longitude'], mergedDF['latitude'])]
+    #create the GDF using the data and the geometry
+    mergedGeoDF = gpd.GeoDataFrame(mergedDF[['date', 'name']], crs=crs, geometry=geometry)
+    return mergedGeoDF
+
+def points_per_day(mergedDF, datetime_input):
+    ''' 
+    Provide a datetime_object and return a geodataframe of of all snow events occuring before and up to then
+    '''
+    selected_data = mergedDF[mergedDF['date'] <= datetime_input]
+    geometry=[Point(longlat_tuple) for longlat_tuple in zip(selected_data['longitude'], selected_data['latitude'])]
+    selectGeoDF = gpd.GeoDataFrame(selected_data[['date', 'name']], crs="epsg:4326", geometry=geometry)
+    return selectGeoDF
+
+def generate_map(datetime_string, mergedDF, year):
+    datetime_obj = datetime.datetime.strptime(datetime_string, "%b %d, %Y")
+    pointMap = points_per_day(mergedDF, datetime_obj)
+    continentalUS = states.boundary.plot(figsize=(15,12), color='grey', linewidth=1)
+    plt.title(f"Stations Having Observed Snow in {year}-{year+1} Season", fontsize=24)
+    plt.text(-125, 25, f"Up to:\n{datetime_string}", fontsize=15)
+    plt.ylabel("Geodetic latitude, north (degree)", fontsize=15)
+    plt.xlabel("Geodetic longitude, east (degree)", fontsize=15)
+    pointMap.plot(ax=continentalUS, markersize=15, color='blue')
+
+def generate_interactive_map(mergedDF, year):
+    #Use this dictionary to make an interactive slider.
+    # The key to be the string formatted date (), the value is the datetime object
+    start_datetime = mergedDF['date'].min()
+    end_datetime = mergedDF['date'].max()
+    string_datetime_mapping = {}
+    current_datetime = start_datetime
+    while current_datetime < end_datetime:
+        string_datetime_mapping[datetime.datetime.strftime(current_datetime, "%b %d, %Y")] = current_datetime
+        current_datetime += datetime.timedelta(days=1)
+    list_of_string_dates = list(string_datetime_mapping.keys())
+    print(f"Select a Date Between {list_of_string_dates[0]} and {list_of_string_dates[-1]}.\n(The map will take a few seconds to update.")
+    dateSlider = ipywidgets.SelectionSlider(options=list_of_string_dates, \
+                                   value=list_of_string_dates[0], \
+                                   description="Date:", \
+                                   layout=ipywidgets.Layout(width='80%'))
+    ipywidgets.widgets.interact(generate_map, datetime_string=dateSlider, mergedDF=ipywidgets.fixed(mergedDF), year=ipywidgets.fixed(year))
+    
+def prompt_for_year():
+    #This function returns an interactive widget object, which has already been rendered.
+    # After calling this function, you just need to access the result of the widget
+    # using dropdownWidget.result
+    from ipywidgets.widgets import Dropdown
+    from ipywidgets import interactive
+    import os
+    from IPython.display import display
+    directory_list = os.listdir("data\Snow_Trends_Data\First-Snow-Dates")
+    available_years = [int(filename.rstrip(".csv")) for filename in directory_list]
+    available_years.sort(reverse=True) #Sort the list from most recent to most distant year
+    dropdownWidget = interactive(lambda Year: Year, Year = available_years)
+    display(dropdownWidget)
+    return dropdownWidget #this is an interactive widget object
